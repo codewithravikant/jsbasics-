d5ecb441a2644428baffed35e63411e006c8f0a7
ravikant <codewithravikant@gmail.com>
2021-09-17 17:36:10 +0300

    day9

---
+++ day9.js
@@ -0,0 +1,257 @@
+
+/*
+Manipulating Complex Objects
+Sometimes you may want to store data in a flexible Data Structure.
+A JavaScript object is one way to handle flexible data. 
+They allow for arbitrary combinations of strings, numbers, booleans, arrays, 
+functions, and objects.
+
+*/
+
+
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+     {
+    "artist": "Daft Punk",
+    "title": "Homework",
+    "release_year": 1997,
+    "formats": [ 
+      "CD",
+      "Cassette",
+      "LP"
+    ],
+    "gold": true
+  }
+  ];
+  
+
+/*
+Accessing Nested Objects
+The sub-properties of objects can be accessed by 
+chaining together the dot or bracket notation.
+
+Here is a nested object:
+
+var ourStorage = {
+  "desk": {
+    "drawer": "stapler"
+  },
+  "cabinet": {
+    "top drawer": { 
+      "folder1": "a file",
+      "folder2": "secrets"
+    },
+    "bottom drawer": "soda"
+  }
+};
+ourStorage.cabinet["top drawer"].folder2;
+ourStorage.desk.drawer;
+ourStorage.cabinet["top drawer"].folder2 would be the string secrets, 
+and ourStorage.desk.drawer would be the string stapler.
+
+Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable.
+Use dot notation for all properties where possible, otherwise use bracket notation.
+
+
+*/
+var myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  var gloveBoxContents = myStorage.car.inside["glove box"];
+
+ /*
+ Accessing Nested Arrays
+
+ objects can contain both nested objects and nested arrays. 
+ Similar to accessing nested objects, array bracket 
+ notation can be chained to access nested arrays.
+
+Here is an example of how to access a nested array:
+
+var ourPets = [
+  {
+    animalType: "cat",
+    names: [
+      "Meowzer",
+      "Fluffy",
+      "Kit-Cat"
+    ]
+  },
+  {
+    animalType: "dog",
+    names: [
+      "Spot",
+      "Bowser",
+      "Frankie"
+    ]
+  }
+];
+ourPets[0].names[1];
+ourPets[1].names[0];
+ourPets[0].names[1] would be the string Fluffy, 
+and ourPets[1].names[0] would be the string Spot.
+
+Using dot and bracket notation, set the variable secondTree to 
+the second item in the trees list from the myPlants object.
+ 
+ */ 
+var myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+  ];
+  
+  var secondTree = myPlants[1].list[1];
+
+
+ /*
+ Record Collection
+You are given an object literal representing a part of your musical album
+ collection. Each album has a unique id number as its key and several other properties.
+  Not all albums have complete information.
+
+You start with an updateRecords function that takes an object literal, records, 
+containing the musical album collection, an id, a prop (like artist or tracks), 
+and a value. Complete the function using the rules below to modify the object passed to the function.
+
+Your function must always return the entire record collection object.
+If prop isn't tracks and value isn't an empty string, update or set that album's prop to value.
+If prop is tracks but the album doesn't have a tracks property, create an empty array and add value to it.
+If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
+If value is an empty string, delete the given prop property from the album.
+Note: A copy of the recordCollection object is used for the tests.
+ 
+ */
+
+// Setup
+var recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+  
+  // Only change code below this line
+  function updateRecords(records, id, prop, value) {
+    if (prop !== 'tracks' && value !== "") {
+      records[id][prop] = value;
+    } else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+      records[id][prop] = [value];
+    } else if (prop === "tracks" && value !== "") {
+      records[id][prop].push(value);
+    } else if (value === "") {
+      delete records[id][prop];
+    }
+    return records;
+  }
+  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+ 
+ /*
+ Iterate with JavaScript While Loops
+You can run the same code multiple times by using a loop.
+
+The first type of loop we will learn is called a while loop because it runs while a 
+specified condition is true and stops once that condition is no longer true.
+
+var ourArray = [];
+var i = 0;
+while(i < 5) {
+  ourArray.push(i);
+  i++;
+}
+In the code example above, the while loop will execute 5 times and 
+append the numbers 0 through 4 to ourArray.
+
+Let's try getting a while loop to work by pushing values to an array.
+
+Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+ 
+ */
+// Setup
+var myArray = [];
+
+// Only change code below this line
+var i = 5;
+while(i >= 0 ) {
+  myArray.push(i);
+  i--;
+}
+/*
+
+The initialization statement is executed one time only before the loop starts. 
+It is typically used to define and setup your loop variable.
+
+The condition statement is evaluated at the beginning of every loop iteration and 
+will continue as long as it evaluates to true. When the condition is false at the start of 
+the iteration, the loop will stop executing. This means if the condition starts as false, 
+your loop will never execute.
+
+The final expression is executed at the end of each loop iteration, prior to the next 
+condition check and is usually used to increment or decrement your loop counter.
+
+In the following example we initialize with i = 0 and iterate while our condition i < 5 is true.
+ We'll increment i by 1 in each loop iteration with i++ as our final expression.
+
+var ourArray = [];
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+ourArray will now have the value [0,1,2,3,4].
+
+Use a for loop to push the values 1 through 5 onto myArray.
+
+*/
+
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for (var i = 1 ; i < 6; i++) {
+  myArray.push(i);
+}
+
